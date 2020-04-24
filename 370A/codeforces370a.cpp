56ced82c025be62e01febc7682a51c2f615b1ff4
irvingpercam <a01703171@itesm.mx>
2020-04-23 19:02:41 -0600

    Upload: Codeforces 370A Partial fix

--- 370A/codeforces370a.cpp
+++ 370A/codeforces370a.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+int diagonal_bishop(int r1, int c1, int r2, int c2){
+	if(((r2 - r1)%(c2 - c1)) != 0) return 0;
+        return abs((c2 - c1) / (r2 - r1));
+}
 int bishop(int r1, int c1, int r2, int c2){
-	int num;
-	if((c2 - c1) == 0 || (r2 - r1) == 0) return 0;
+	if(r1 == r2 && c1 != c2 && diagonal_bishop(r1,c1,r2,c2) != 0) return abs(c2 - c1);
+	if(r1 != r2 && c1 == c2 && diagonal_bishop(r1,c1,r2,c2) != 0) return abs(r2 - r1);
 	else
-		num = abs((c2 - c1)/(r2 - r1));
-		return num;
+		return diagonal_bishop(r1,c1,r2,c2);
 }
 int rook(int r1, int c1, int r2, int c2){
 	if(r2 == r1 && c2 == c1) return 0;
